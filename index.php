be44434252ebd70b2c842939600bac1c88aa827a
Luiz Fernando - TNB <luiz.is@thenewblack.me>
2015-05-15 13:12:16 -0300

    Primeiro commit do projeto1

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ * @author Luiz Fernando Lidio | The New Black Studio
+ * @powered by: http://www.thenewblack.me
+ * 
+ *            .=     ,        =.
+ *   _  _   /'/    )\,/,/(_   \ \
+ *    `//-.|  (  ,\\)\//\)\/_  ) |
+ *    //___\   `\\\/\\/\/\\///'  /
+ * ,-"~`-._ `"--'_   `"""`  _ \`'"~-,_      Múúúúúúúúúúúúúúú!
+ * \       `-.  '_`.      .'_` \ ,-"~`/     Hier gibt's nichts zu sehen!!!
+ *  `.__.-'`/   (-\        /-) |-.__,'
+ *    ||   |     \O)  /^\ (O/  |
+ *    `\\  |         /   `\    /
+ *      \\  \       /      `\ /
+ *       `\\ `-.  /' .---.--.\
+ *         `\\/`~(, '()      ('
+ *          /(O) \\   _,.-.,_)
+ *         //  \\ `\'`      /
+ *        / |  ||   `""""~"`
+ *      /'  |__||
+ *             `o 
+ * 
+ * 
+ */
+?>
+
+<?php require_once "head.php"; ?>
+
+<div class="container">
+    <div class="row">
+        <?php require_once "menu.php"; ?>
+    </div>
+</div>
+
+    <div class="container">
+        <div class="row">
+            <?php
+                if(file_exists($_GET["arquivo"])){
+                    require_once ($_GET["arquivo"]);
+                }
+                else{
+                    require_once ("404.php");
+                }
+            ?>
+        </div>
+    </div>
+
+<?php require_once "footer.php"?>
---
+++ menu.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ * @author Luiz Fernando Lidio | The New Black Studio
+ * @powered by: http://www.thenewblack.me
+ * 
+ *            .=     ,        =.
+ *   _  _   /'/    )\,/,/(_   \ \
+ *    `//-.|  (  ,\\)\//\)\/_  ) |
+ *    //___\   `\\\/\\/\/\\///'  /
+ * ,-"~`-._ `"--'_   `"""`  _ \`'"~-,_      Múúúúúúúúúúúúúúú!
+ * \       `-.  '_`.      .'_` \ ,-"~`/     Hier gibt's nichts zu sehen!!!
+ *  `.__.-'`/   (-\        /-) |-.__,'
+ *    ||   |     \O)  /^\ (O/  |
+ *    `\\  |         /   `\    /
+ *      \\  \       /      `\ /
+ *       `\\ `-.  /' .---.--.\
+ *         `\\/`~(, '()      ('
+ *          /(O) \\   _,.-.,_)
+ *         //  \\ `\'`      /
+ *        / |  ||   `""""~"`
+ *      /'  |__||
+ *             `o 
+ * 
+ * 
+ */
+?>
+
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/?arquivo=home.php">PROJETO 1</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li><a href="/?arquivo=home.php">HOME</a></li>
+                <li><a href="/?arquivo=empresa.php">EMPRESA</a></li>
+                <li><a href="/?arquivo=produtos.php">PRODUTOS</a></li>
+                <li><a href="/?arquivo=servicos.php">SERVIÇOS</a></li>
+                <li><a href="/?arquivo=contato.php">CONTATO</a></li>
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
